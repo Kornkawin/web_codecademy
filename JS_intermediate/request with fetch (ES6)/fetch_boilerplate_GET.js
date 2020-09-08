6f53a0deb57400ae6264cd8198cc6bc0dc58127c
Kornkawin Bijayayothin <k.bijayayothin@gmail.com>
2020-09-08 13:04:24 +0700

    update 03-Sep-2020

---
+++ JS_intermediate/request with fetch (ES6)/fetch_boilerplate_GET.js
@@ -0,0 +1,15 @@
+/*
+  The second .then()‘s success callback won’t run until the previous .then() method
+  has finished running. It will also not run if there was an error was thrown previously.
+*/
+
+fetch('https://api-to-call.com/endpoint').then(response=>{
+  if(response.ok){
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError=>{
+  console.log(networkError.message);
+}).then(jsonResponse=>{
+  return jsonResponse;
+});
